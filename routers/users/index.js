60ddcd3d0fb7502fd342b3273621eea1f7ad2489
khm0813 <khm.commin@gmail.com>
2021-12-29 17:21:55 +0900

    router 활용

--- routers/users/index.js
+++ routers/users/index.js
@@ -5,6 +5,7 @@ const controller = require('./users.controller');
 
 
 router.get('/', controller.users)
+router.get('/list', controller.user_list)
 
 module.exports = router;
 
--- routers/users/users.controller.js
+++ routers/users/users.controller.js
@@ -1,3 +1,8 @@
 exports.users = (req, res, next) => {
     res.send('response with a resource')
 }
+
+
+exports.user_list = (req, res, next) => {
+    res.send('response with a resource list')
+}
