8e93f5df1a1991a6f075ec713be673ce7891c2ac
khm0813 <khm.commin@gmail.com>
2022-01-07 10:21:34 +0900

    study nodejs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # backend-nodejs-fw
 Commins NodeJS Framework
+
+> 미션
+> 1. express 설정
+> 2. 필요한 npm 모듈 설치 (express, pug(view engine),body-parser,mysql )
+> 3. input UI 만들기 (검색창)
+> 4. 검색결과 받아서 dummy json 형태를 내려주기
+> 5. 화면에 결과를 노출하기
---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var app = express(); // 1. express app
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+const { json } = require('express/lib/response');
+
+
+
+// static 폴더 지정
+app.use(express.static('public'));
+
+// body-parser 연결
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// mysql 연결
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 13306,
+    user: 'danal',
+    password: 'khm0813',
+    database: 'my_db',
+})
+
+
+app.set('view engine', 'ejs');
+
+// 서버 포트 열기
+app.listen(3001, function () {
+    console.log('start node js express server port on 3001')
+})
+
+// 서버 포트 다른 포트로 열기
+app.listen(3002, function () {
+    console.log('start node js express server port on 3002')
+})
+
+app.get('/', function (req, res) {
+    console.log("hello node", __dirname);
+    // res.send("Hello Node")
+    // res.sendFile(__dirname + '/public/main.html')
+    res.render('index', { title: 'this is title', message: 'Hello Pug!!' })
+})
+
+app.post('/search_result', function (req, res) {
+    // console.log('search_result' , req.body)
+    console.log(JSON.stringify(req.body))
+    var requestData = JSON.stringify(req.body);
+
+
+
+    var responseData = {};
+
+    console.log(req.body.search_input)
+
+    var query = connection.query('select name,email from users where email like "%' + req.body.search_input + '%" or name like "%' + req.body.search_input + '%"   ', function (err, rows) {
+        if (err) throw err;
+        if (rows) {
+            console.log(rows)
+            responseData.result = 'success';
+            responseData.datas = [];
+            for (var i = 0; i < rows.length; i++)
+                responseData.datas.push({ 'name': rows[i].name, 'email': rows[i].email })
+
+            console.log("result ", responseData)
+            res.json(responseData)
+
+        } else {
+            console.log('None')
+            responseData.result = 'fail';
+            responseData.datas = undefined;
+
+            res.json(responseData)
+        }
+    })
+})
