1bab156e99c2fb72ced1fd8243d9af7c3b194a2c
khm0813 <khm.commin@gmail.com>
2022-01-11 12:25:42 +0900

    bcrypt version up

---
+++ dbconnect_test.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://khm0813:khm0813@commin-pg-mongo.0uj4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log(err)
+    // perform actions on the collection object
+    client.close();
+});
